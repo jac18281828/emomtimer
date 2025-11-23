b17823d700d0428a7f7fc921654b1c088d75df34
John Cairns <john@2ad.com>
2025-11-22 20:14:45 -0600

    chore: mobile improvements

--- src/main.rs
+++ src/main.rs
@@ -294,7 +294,7 @@ impl Component for App {
             <head>
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="theme-color" content="#667eea" />
